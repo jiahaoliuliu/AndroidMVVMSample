edde8ba1388736e0d52d2e965e4eeeea309817e8
Jiahao Liu <jiahaoliuliu@gmail.com>
2024-01-02 19:33:46 +0100

    JL Added the repository

---
+++ app/src/main/java/com/jiahaoliuliu/androidmvvmsample/data/repository/TopHeadlineRepository.kt
@@ -0,0 +1,20 @@
+package com.jiahaoliuliu.androidmvvmsample.data.repository
+
+import com.jiahaoliuliu.androidmvvmsample.data.api.NetworkService
+import com.jiahaoliuliu.androidmvvmsample.data.model.Article
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TopHeadlineRepository @Inject constructor(private val networkService: NetworkService) {
+    fun getTopHeadlines(country: String): Flow<List<Article>> {
+        return flow {
+            emit(networkService.getTopHeadlines(country))
+        }.map {
+            it.articles
+        }
+    }
+}
