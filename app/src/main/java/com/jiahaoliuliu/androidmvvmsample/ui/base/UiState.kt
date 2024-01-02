8e0b09b18bbfbc0562e00882a3041abd700ce210
Jiahao Liu <jiahaoliuliu@gmail.com>
2024-01-02 19:39:52 +0100

    JL created viewmodel provider factory

---
+++ app/src/main/java/com/jiahaoliuliu/androidmvvmsample/ui/base/UiState.kt
@@ -0,0 +1,7 @@
+package com.jiahaoliuliu.androidmvvmsample.ui.base
+
+sealed interface UiState<out T> {
+    data class Success<T>(val data: T): UiState<T>
+    data class Error(val message: String): UiState<Nothing>
+    data object Loading: UiState<Nothing>
+}
