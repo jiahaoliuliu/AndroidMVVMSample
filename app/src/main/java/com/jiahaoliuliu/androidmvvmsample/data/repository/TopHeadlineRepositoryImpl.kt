02d70084761a4eda5c36b41b5ee88deb55193864
Jiahao Liu <jiahaoliuliu@gmail.com>
2024-01-16 12:44:36 +0100

    JL Removed unused import

--- app/src/main/java/com/jiahaoliuliu/androidmvvmsample/data/repository/TopHeadlineRepositoryImpl.kt
+++ app/src/main/java/com/jiahaoliuliu/androidmvvmsample/data/repository/TopHeadlineRepositoryImpl.kt
@@ -7,7 +7,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
-import javax.inject.Singleton
 
 class TopHeadlineRepositoryImpl @Inject constructor(private val networkService: NetworkService): TopHeadlineRepository {
     override fun getTopHeadlines(country: String): Flow<List<ArticleRemoteData>> {
